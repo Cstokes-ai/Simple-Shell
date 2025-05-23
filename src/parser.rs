1bfd0bc7a7577722d2ad508921e84fb6ee2e42e1
Cornell Stokes <cdsgqc@umsystem.edu>
2025-05-23 14:42:05 -0500

    First commit for shell project

---
+++ src/parser.rs
@@ -0,0 +1,44 @@
+// do this file first
+
+/*
+parser.rs
+Responsibility: Handles user input and breaks it down into usable components.
+What it does:
+
+Reads input from standard input.
+
+Strips whitespace and newline characters.
+
+Splits the input into command and arguments (usually a Vec<String>).
+
+Prepares the command in a format that executor.rs can use.
+
+No execution logic happens here — only parsing and sanitization.
+
+*/
+
+
+pub fn user_input()-> String {
+    let mut input = String:: new();
+    std:: io::stdiin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    let input = input.trim(); // Remove whitespace and newline characters
+    let input = input.to_string();
+    //now we need to split input into command line arguments using Vec<String>
+    let args: Vec<String> = input
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect();
+
+    //this gets the commands ready in a format for my executor..rs
+    
+    let mut command = String::new();
+    for args in args.iter() {
+        command.push_str(args);
+        command.push(' ');
+    }
+    command.pop();
+    
+
+}
